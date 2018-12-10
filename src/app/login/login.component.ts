e74a4e0cf151b1948b435bf6053811c28db06d33
Antonio Aguila <aguilantonio16@gmail.com>
2018-12-09 23:50:32 -0600

    register,login,googleSignIn,logout added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -10,15 +14,87 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router ) { }
+  email:string;
+  recuerdame:boolean =false;
+
+  auth2: any;
+
+  constructor( public router: Router, public _usuarioService: UsuarioService ) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+    //load remember me
+    this.email = localStorage.getItem('email')  || '';
+    if(this.email.length > 1){
+      this.recuerdame = true;
+    }
+
   }
 
-  ingresar() {
 
-    this.router.navigate([ '/dashboard' ]);
+  googleInit() {
+
+    /*Initializes Signin v2 and sets up listeners. */
+    gapi.load('auth2', () => {
+
+      this.auth2 = gapi.auth2.init({
+        client_id: '539988727082-3kkkap7e4oeitbpp59qot85kvelh78mn.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+    });
+
+    this.attachSignin( document.getElementById('btnGoogle'));
+
+    });
+  }
+
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+     /*  let profile = googleUser.getBasicProfile();
+      console.log(profile);
+      console.log('ID: ' + profile.getId()); 
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); */
+
+      let token = googleUser.getAuthResponse().id_token;
+      //console.log(token);
+
+      this._usuarioService.loginGoogle( token )
+          //.subscribe( resp => { console.log(resp);  });
+          .subscribe( () => window.location.href = '#/dashboard' );
+     
+
+    });
+
+  }
+
+
+
+  ingresar( forma: NgForm) {
+
+
+
+    let usuario = new Usuario(null,forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame)
+    .subscribe( correcto => this.router.navigate(['/dashboard']));
+
+   /*  this._usuarioService.login(usuario, forma.value.recuerdame)
+    .subscribe( correcto => {
+
+      console.log(correcto);
+
+    }); */
+
+   /*  console.log(forma.valid);
+    console.log(forma.value); */
+
+    //this.router.navigate([ '/dashboard' ]);
 
   }
 
---
+++ src/app/services/guards/login-guard.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router} from '@angular/router';
+import { UsuarioService } from '../usuario/usuario.service';
+
+
+@Injectable(/* {
+  providedIn: 'root'
+} */)
+export class LoginGuardGuard implements CanActivate {
+
+  constructor( public _usuarioService: UsuarioService, public router: Router ) {}
+
+  canActivate() {
+
+    console.log('pasa por login guard');
+
+    if(this._usuarioService.estalogueado()){
+      console.log('pasa el guard');
+      return true;
+    } else {
+      console.log('bloqueado por el guard');
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    //return true;
+  }
+}
